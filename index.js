53d74dc62ebe88ca97695173fd8febfbfc9f56fe
Airin Akter <airinmoni.bd@gmail.com>
2024-06-08 12:12:55 +0600

    applied trainer database modify

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
@@ -52,7 +52,7 @@ async function run() {
     // middlewares
     const verifyToken = (req, res, next) => {
       //console.log('inside verify token', req.headers);
-      console.log('inside verify token', req.headers.authorization);
+      //console.log('inside verify token', req.headers.authorization);
       if(!req.headers.authorization){
         return res.status(401).send({message: 'unauthorized access'});
       }
@@ -65,6 +65,29 @@ async function run() {
         req.decoded = decoded;
         next();
       });
+    }
+    // use verify admin after verify token
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({messsage: 'forbidden access'})
+      }
+      next();
+    }
+
+    // use verify admin after verify token
+    const verifyTrainer = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isTrainer = user?.role === 'trainer';
+      if(!isTrainer){
+        return res.status(403).send({messsage: 'forbidden access'})
+      }
+      next();
     }
 
 
@@ -79,19 +102,27 @@ async function run() {
       if(existingUser){
         return res.send({message: 'user already exists', insertedId: null})
       }
-      console.log(user);
+      //console.log(user);
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
     //get all users
-    app.get('/users', verifyToken, async(req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async(req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
+    // delete user
+    app.delete('/users/:id', verifyToken, verifyAdmin, async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+    
 
-    // make role admin update
-    app.patch('/users/admin/:id', verifyToken, async(req, res) => {
+    // make role admin update ------------------------------------------------------------------------
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async(req, res) => {
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)}
       const updateDoc = {
@@ -102,7 +133,7 @@ async function run() {
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     })
-    // get admin
+
     app.get('/users/admin/:email', verifyToken, async(req, res) => {
       const email = req.params.email;
       if(email !== req.decoded.email) {
@@ -110,6 +141,7 @@ async function run() {
       }
       const query = {email: email}
       const user = await userCollection.findOne(query);
+      //console.log({user});
       let admin = false;
       if (user){
         admin = user?.role === 'admin';
@@ -117,9 +149,16 @@ async function run() {
       res.send({admin});
     })
 
+    
+
     // Be a trainer related api
     app.post('/betrainer', verifyToken, async(req, res) => {
       const trainer = req.body;
+      const query = {email:trainer.email}
+      const existingUser = await trainerCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exists', insertedId: null})
+      }
       const result = await trainerCollection.insertOne(trainer);
       res.send(result);
     })
@@ -127,6 +166,74 @@ async function run() {
     app.get('/betrainer', async(req, res) => {
       const result = await trainerCollection.find().toArray();
       res.send(result);
+    })
+    // get single id
+    app.get('/betrainer/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await trainerCollection.findOne(query);
+      //console.log(result);
+      res.send(result)
+    })
+    app.patch('/betrainer/:id', verifyToken, async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'trainer',
+          status: 'approved',          
+        }
+      }
+      const result = await trainerCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    // delete user
+    app.delete('/users/:id', verifyToken, verifyAdmin, async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.patch('/betrainer/role/:id', verifyToken, async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'user'
+        }
+      }
+      const result = await trainerCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    // make role trainer update ------------------------------------------------------------------------
+    app.patch('/betrainer/trainer/:id', verifyToken, verifyTrainer, async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'trainer'
+        }
+      }
+      const result = await trainerCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    app.get('/betrainer/trainer/:email', verifyToken, async(req, res) => {
+      const email = req.params.email;
+      if(email !== req.decoded.email) {
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      const query = {email: email}
+      const user = await userCollection.findOne(query);
+      //console.log({user});
+      let trainer = false;
+      if (user){
+        trainer = user?.role === 'trainer';
+      }
+      res.send({trainer});
     })
 
   
